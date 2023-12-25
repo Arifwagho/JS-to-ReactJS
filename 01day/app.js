e1354b2a59375a4c1bf86ab400771e4e0cd07573
Arifwagho <wagho.siba@gmail.com>
2023-12-25 11:23:37 +0500

    Work complete

---
+++ 01day/app.js
@@ -0,0 +1,9 @@
+const userId = 12345;
+let userName = 'Arif';
+let userCity = 'karachi';
+let userEmail = 'arif@google.com';
+// userId = 141414;
+let userStatus 
+
+
+console.table([userId,userName,userEmail,userCity ,userStatus]);
